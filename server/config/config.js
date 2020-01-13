c8635f102af5e19e54b4b77852ca5368a51d4cf6
= <=>
2020-01-13 00:12:42 -0400

    crud de usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,19 @@
 
 //PUERTO:
 process.env.PORT = process.env.PORT || 3000;
+
+//ENTORNO
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//BASE DE DATOS
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://localhost:27017/cafe'
+}
+else
+{
+    urlDB = 'mongodb+srv://admin:46TUwfotvFuEoeeL@cluster0-sadea.mongodb.net/cafe?retryWrites=true&w=majority&ssl=false';
+}
+
+process.env.URLDB = urlDB;
