b2feac72e95f80f5b565fa92300d54c7260960d6
= <=>
2020-01-15 23:52:25 -0400

    jwt

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,13 @@ process.env.PORT = process.env.PORT || 3000;
 //ENTORNO
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+//VENCIMIENTO DEL TOKEN
+//60seg * 60min * 24h * 30d
+process.env.CADUCIDAD_TOKEN = 60*60*24*30;
+
+//SEED DE AUTENTICACION
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
 //BASE DE DATOS
 let urlDB;
 
---
+++ server/routes/index.js
@@ -0,0 +1,13 @@
+//imports
+const express = require('express');
+const app = express();
+
+
+//routes
+app.use( require('./usuario') );
+app.use( require('./login') );
+
+
+
+
+module.exports = app;
